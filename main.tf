1f43dece758a7a5583fc0f797dc83eac8c41eb4c
endritrugova <endrit@gmail.com>
2025-03-06 19:46:05 +0100

    prevent_deletion_if_contains_resources = false

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ terraform {
 provider "azurerm" {
   features {}
   skip_provider_registration = true
+  prevent_deletion_if_contains_resources = false
 }
 
 resource "random_string" "uniquestring" {
